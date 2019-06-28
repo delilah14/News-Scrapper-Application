f6ec0fc6b78f505cb9a4a3afb08696ef9b9bc72e
Abigail <Abigiya24@gmail.com>
2019-06-28 15:22:39 +0300

    html and css

--- news/src/main/java/com/iyoa/news/NewsApplication.java
+++ news/src/main/java/com/iyoa/news/NewsApplication.java
@@ -2,12 +2,27 @@ package com.iyoa.news;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 public class NewsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsApplication.class, args);
+	}
+	
+	@Controller
+	public class Home {
+		
+		@RequestMapping(value = "/News")
+	    public ModelAndView getDateAndTime() {
+	        return new ModelAndView("News");
+
+		}
+		
+		
 	}
 
 }
